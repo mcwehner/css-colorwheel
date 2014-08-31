59bc7b4b98e4bcad68508f1c413bb0c2d94806b0
Michael Wehner <mcwehner@gmail.com>
2014-08-30 19:00:23 -0700

    Initial commit.

---
+++ Gemfile
@@ -0,0 +1,13 @@
+source 'https://rubygems.org'
+
+group :development do
+  gem 'better_errors'
+  gem 'binding_of_caller'
+  gem 'pry-rails'
+end
+
+gem 'activesupport'
+gem 'autoprefixer-rails'
+gem 'rerun'
+gem 'sinatra'
+gem 'thin'
---
+++ server.rb
@@ -0,0 +1,35 @@
+#!/usr/bin/env ruby
+
+require 'bundler/setup'
+require 'sinatra'
+
+require 'autoprefixer-rails'
+
+# Useful core extensions.
+# require 'active_support'
+# require 'active_support/core_ext'
+
+# Additional time support.
+# require 'time'
+
+
+configure do
+  set :application_title, 'sinatra-scaffold'
+  set :assets_path,       "#{File.dirname(__FILE__)}/assets"
+end
+
+helpers do
+end
+
+# Routes
+get '/' do
+  erb :index
+end
+
+get '/stylesheets/:name' do
+  # SECURE: Potentially dangerous filesystem access?
+  filename = "#{settings.assets_path}/stylesheets/#{File.basename(params[:name])}"
+
+  content_type :css
+  AutoprefixerRails.process(File.read(filename)).css
+end
